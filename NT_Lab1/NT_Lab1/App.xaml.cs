713c21b197735845b3e36596c68dcfc644e0e090
yulis araujo <yulianaraujo@hotmail.com>
2020-02-12 11:35:24 -0500

    A

---
+++ NT_Lab1/NT_Lab1/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using Xamarin.Forms;
+
+namespace NT_Lab1
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+
+            MainPage = new MainPage()
+            {
+                Title = "Realizar Conteo concurzo"
+            };
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ NT_Lab1/NT_Lab1/MainPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Threading;
+using Xamarin.Forms;
+
+namespace NT_Lab1
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private const int V = 00;
+        int count = 0;
+        static long dd = 0, hh = 0, mm = 0, ss = 0, ms=0;
+        static Stopwatch stopwatch = new Stopwatch();
+        System.Timers.Timer Timer1 = new System.Timers.Timer();
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            btnIniciarC.Clicked += BtnIniciarC_Clicked;
+
+        }
+
+        private void BtnIniciarC_Clicked(object sender, EventArgs e)
+        {
+            count++;
+            Pconteo.Text = count.ToString();
+        }
+
+        
+     
+
+        }
+
+       
+    }
+
